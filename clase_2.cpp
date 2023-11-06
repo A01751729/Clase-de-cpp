9030928806858b06f054a0eb8dfaac112fc5a069
AndresMendez <126106627+A01751729@users.noreply.github.com>
2023-11-06 16:46:23 +0000

    clase 2

---
+++ clase_2.cpp
@@ -0,0 +1,39 @@
+# include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void metodo(int x, int &y, int *z){
+  x=x+5;
+  y=y+10;
+  *z=*z+100;
+  }
+
+int main(){
+  
+  int arreglo[3]={10,20,30};
+  cout<<arreglo[1]<<endl;
+  
+  int *aparr=&arreglo[0];
+  
+  for (int i =0; i<3; i++)
+  {
+    cout<<aparr<<"  "<<*aparr<<endl;
+    aparr++;
+    }
+  
+  int x=10;
+  int y=20;
+  
+  int *ap= &x;
+
+  cout<<ap<<endl;
+  cout<<*ap<<endl;
+  
+  //metodo(x,y, ap);
+  
+  cout<<x<<"  "<<y<<endl;
+  
+  return 0;
+  }
